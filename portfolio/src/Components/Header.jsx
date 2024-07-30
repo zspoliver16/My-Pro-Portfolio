f1478f00d44a6513568be9433bd934f2a5546b81
zachary oliver <zspoliver@gmail.com>
2024-07-30 13:14:14 -0400

    added header

---
+++ portfolio/src/Components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <div className="header">
+                <h1>Zachary Oliver</h1>
+                <nav>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
