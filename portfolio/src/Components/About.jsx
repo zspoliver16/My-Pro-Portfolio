bd97ef24dbafbf34118b87c2163f5d209192cfdc
zachary oliver <zspoliver@gmail.com>
2024-08-13 10:12:12 -0400

    created about section

---
+++ portfolio/src/Components/About.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+
+function About() {
+    return (
+        <div className="about">
+            <h2>About Me</h2>
+            <p></p>
+        </div>
+    );
+}
+
+export default About;
